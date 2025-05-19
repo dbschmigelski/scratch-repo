0603ba4be75109637a97066fb4458173417dc851
Dean Schmigelski <dbschmigelski+github@gmail.com>
2025-05-19 16:08:34 -0400

    Update low_level_mcp.py

--- low_level_mcp.py
+++ low_level_mcp.py
@@ -1,4 +1,5 @@
 from mcp import stdio_client, StdioServerParameters
+import asyncio
 
 async def start_server():
   client = stdio_client(StdioServerParameters(command="uvx", args=["awslabs.aws-documentation-mcp-server@latest"]))
