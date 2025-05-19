99af247c40992e7a4e0d8042eda407f755d7d8e6
Dean Schmigelski <dbschmigelski+github@gmail.com>
2025-05-19 16:11:08 -0400

    Update low_level_mcp.py

--- low_level_mcp.py
+++ low_level_mcp.py
@@ -4,9 +4,10 @@ import asyncio
 async def start_server():
   client = stdio_client(StdioServerParameters(command="uvx", args=["awslabs.aws-documentation-mcp-server@latest"]))
 
-  async with client:
-    await client.initialize()
-    print("INITIALIZED")
+  async with client as (read_stream, write_stream):
+    async with ClientSession(read_stream, write_stream) as session:
+        await session.initialize()
+        print("INITIALIZED")
 
 if __name__ == "__main__":
     asyncio.run(start_server())
